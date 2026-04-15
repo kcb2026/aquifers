ee440b185d07ceb8eabeddfa88a9d666d1f3d27f
kcb2026 <kate.bethel@wsu.edu>
2026-04-14 17:16:40 -0700

    Add files via upload

---
+++ qgis2web_2026_04_14-17_05_36_575822/layers/layers.js
@@ -0,0 +1,42 @@
+var wms_layers = [];
+
+
+        var lyr_ESRIPhysical_0 = new ol.layer.Tile({
+            'title': 'ESRI Physical',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}'
+            })
+        });
+var format_sourcedatafig2_1 = new ol.format.GeoJSON();
+var features_sourcedatafig2_1 = format_sourcedatafig2_1.readFeatures(json_sourcedatafig2_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_sourcedatafig2_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_sourcedatafig2_1.addFeatures(features_sourcedatafig2_1);
+var lyr_sourcedatafig2_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_sourcedatafig2_1, 
+                style: style_sourcedatafig2_1,
+                popuplayertitle: 'sourcedatafig2',
+                interactive: true,
+    title: 'sourcedatafig2<br />\
+    <img src="styles/legend/sourcedatafig2_1_0.png" /> -2.581 - -0.015<br />\
+    <img src="styles/legend/sourcedatafig2_1_1.png" /> -0.015 - 0.016<br />\
+    <img src="styles/legend/sourcedatafig2_1_2.png" /> 0.016 - 0.078<br />\
+    <img src="styles/legend/sourcedatafig2_1_3.png" /> 0.078 - 0.303<br />\
+    <img src="styles/legend/sourcedatafig2_1_4.png" /> 0.303 - 2.951<br />' });
+
+lyr_ESRIPhysical_0.setVisible(true);lyr_sourcedatafig2_1.setVisible(true);
+var layersList = [lyr_ESRIPhysical_0,lyr_sourcedatafig2_1];
+lyr_sourcedatafig2_1.set('fieldAliases', {'ogc_fid': 'ogc_fid', 'm_per_year': 'Meters Change per Year', 'aquifer': 'Aquifer Name', 'broader': 'broader', });
+lyr_sourcedatafig2_1.set('fieldImages', {'ogc_fid': 'Hidden', 'm_per_year': 'TextEdit', 'aquifer': 'TextEdit', 'broader': 'Hidden', });
+lyr_sourcedatafig2_1.set('fieldLabels', {'m_per_year': 'inline label - visible with data', 'aquifer': 'inline label - visible with data', });
+lyr_sourcedatafig2_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ qgis2web_2026_04_14-17_05_36_575822/styles/sourcedatafig2_1_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_sourcedatafig2_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("m_per_year");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= -2.581250 && value <= -0.014659) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(237,79,21,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= -0.014659 && value <= 0.016115) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(246,167,138,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.016115 && value <= 0.077868) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,255,255,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.077868 && value <= 0.303103) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(158,167,238,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.303103 && value <= 2.951098) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(60,80,220,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
